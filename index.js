56f0e05585a31a8cd4fc249bf48e1ec9ad9bce69
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-06-17 20:32:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+/*const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+*/
+
+ function superbowlWin(arrayObject){
+
+    for(const item of arrayObject){
+        let findWinYear=arrayObject.find(a=>a.result=="W")
+
+        if (findWinYear==undefined){
+            return console.log('undefined')
+        }
+
+        else{
+            return findWinYear.year
+        }
+        
+    }
+
+  }
+
+  superbowlWin(record)
